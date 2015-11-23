7da102d2c9c61cf5991250eb27363b8869b52003
Paulo Martins <paulovmr@gmail.com>
2015-11-23 00:18:00 -0200

    testing request and application scopes

--- src/main/java/com/sandbox/users/UserDAO.java
+++ src/main/java/com/sandbox/users/UserDAO.java
@@ -1,8 +1,21 @@
 package com.sandbox.users;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.enterprise.context.RequestScoped;
+
+@RequestScoped
 public class UserDAO {
+	
+	private Date date;
+		
+	public UserDAO() {
+		date = new Date();
+	}
 
 	public String searchUser() {
-		return "bbb";
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss.SSS");
+		return "data: " + sdf.format(date);
 	}
 }
